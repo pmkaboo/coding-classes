1f2947b269ccc73db794d9936ffbcce4bc6c9a54
Jakub Krchlík <j.krchlik@gmail.com>
2023-01-26 10:01:13 +0100

    stock change fix

--- level2/restaurants/kubo_restaurants.rb
+++ level2/restaurants/kubo_restaurants.rb
@@ -19,15 +19,17 @@ class Restaurant
 
   def place_order(order_items, discount: 0)
     order_price = 0
+    checked_items = []
     order_items.each do |order_item|
       return DOUBLE_DISCOUNT_ERROR if order_item[:discount] && discount.positive?
 
       checked_item = @items.find { |i| i.name == order_item[:name] }
       return UNAVAILABLE_ITEM_ERROR unless checked_item&.in_stock?(order_item[:count])
 
-      checked_item.stock -= order_item[:count]
+      checked_items << checked_item
       order_price += checked_item.price * order_item[:count] * (1 - order_item[:discount].to_f)
     end
+    checked_items.each { |item| item.remove_from_stock(order_items) }
     order_price * (1 - discount.to_f)
   end
 end
@@ -41,6 +43,10 @@ class Item
     @name = name
     @price = price
     @stock = stock
+  end
+
+  def remove_from_stock(order_items)
+    @stock -= order_items.find { |i| i[:name] = @name }[:count]
   end
 
   def in_stock?(count)
