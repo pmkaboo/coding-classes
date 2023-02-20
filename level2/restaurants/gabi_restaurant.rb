7f0a00dd3130cc18531df4265a4372bf6c2ed1af
Gabriela Pusicova <gabriela.pusicova@reeftechnology.com>
2023-02-20 17:28:56 +0100

    simplified placed_orders, fixed stock depletion, fixed formatting

--- level2/restaurants/gabi_restaurant.rb
+++ level2/restaurants/gabi_restaurant.rb
@@ -1,86 +1,77 @@
-
-require 'pry'
-
-UNAVAILABLE_ITEM_ERROR = 'Items not available'
-DOUBLE_DISCOUNT_ERROR = 'Double discount is not allowed'
+# frozen_string_literal: true
 
 class Restaurant
+  UNAVAILABLE_ITEM_ERROR = 'Items not available'
+  DOUBLE_DISCOUNT_ERROR = 'Double discount is not allowed'
+  attr_reader :name, :items
 
-    def initialize(name,*items)
-        @name = name.to_s
-        @items = items
+  def initialize(name, *items)
+    @name = name.to_s
+    @items = items
+  end
+
+  def add_items(*items)
+    items.each { |i| @items << i }
+  end
+
+  def remove_item(item_name)
+    @items.delete_if { |item| item.name == item_name }
+  end
+
+  def place_order(ordered_items, discount: 0)
+    order_total = 0
+    items_in_stock = []
+
+    ordered_items.each do |ordered_item|
+      item = find_item(ordered_item)
+
+      return UNAVAILABLE_ITEM_ERROR if item.nil? || item_out_of_stock?(item, ordered_item)
+      return DOUBLE_DISCOUNT_ERROR if double_discount?(ordered_item, discount)
+
+      order_total += calculate_total(item, ordered_item)
+      items_in_stock << { item:, quantity: ordered_item[:count] }
     end
-        
-    def name
-        @name
+
+    deplete_stock(items_in_stock)
+    order_total *= 1 - discount
+  end
+
+  def find_item(ordered_item)
+    @items.find { |item| item.name == ordered_item[:name] }
+  end
+
+  def item_out_of_stock?(item, ordered_item)
+    item.stock < ordered_item[:count]
+  end
+
+  def double_discount?(ordered_item, discount)
+    ordered_item[:discount] && discount > 0
+  end
+
+  def calculate_total(item, ordered_item)
+    price = item.price
+    quantity = ordered_item[:count]
+    item_discount = ordered_item[:discount] || 0
+    price * quantity * (1 - item_discount)
+  end
+
+  def deplete_stock(items_in_stock)
+    items_in_stock.each do |item|
+      item[:item].deplete_stock(item[:quantity])
     end
-
-    def items
-        @items
-    end
-
-    def add_items(*item)
-        item.each{|i| @items << i}
-    end
-
-    def remove_item(item_name)
-        item = @items.find{|item| item.name == item_name}
-        @items.delete(item)
-    end
-
-    def place_order(ordered_items, discount: 0)
-        order_total = 0
-        ordered_items.each do |ordered_item| 
-            #binding.pry
-            item = @items.find{|item| item.name == ordered_item[:name]}
-            return UNAVAILABLE_ITEM_ERROR if item.nil?
-
-            price = item.price
-            stock = item.stock
-            quantity = ordered_item[:count]
-            if stock >= quantity
-                item_discount = ordered_item[:discount] || 0
-                return DOUBLE_DISCOUNT_ERROR if discount != 0 && item_discount != 0
-
-                total = price * quantity * (1 - item_discount)
-                order_total += total
-                
-                item.deplete_stock(quantity)
-            else
-                return UNAVAILABLE_ITEM_ERROR
-            end
-            
-        end 
-        order_total *= 1 - discount
-        return order_total
-    end
+  end
 end
-
 
 class Item
+  attr_reader :name, :price, :stock
 
-    def initialize(name,price,stock)
-        @name = name.to_s
-        @price = price.to_f
-        @stock = stock.to_f
-    end
+  def initialize(name, price, stock)
+    @name = name.to_s
+    @price = price.to_f
+    @stock = stock.to_f
+  end
 
-    def name
-        @name
-    end
-
-    def price
-        @price
-    end
-
-    def stock
-        @stock
-    end
-
-    def deplete_stock(quantity)
-        @stock -= quantity
-    end
+  def deplete_stock(quantity)
+    @stock -= quantity
+  end
 end
-
-
-
